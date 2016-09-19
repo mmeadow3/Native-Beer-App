53cd3da7ed33b8ad6dc71e6eb8ad3d12a5a042fa
Mike Meadows <mmeadow33@gmail.com>
2016-09-18 20:59:43 -0500

    got api call to work and return

--- apiCall.js
+++ apiCall.js
@@ -1,21 +1,46 @@
 "use strict";
 
 let request = require("request");
+let rp = require("request-promise");
 const mikeKey = require("./apiKey")
 
 // let beer = "sculpin"
-let brewery = "yuengling"
+// let brewery = "yuengling"
 
-function apiCallBeer(beer) {request("https://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=" + mikeKey + "&format=json",
-function (error, response, body){
-  body = JSON.parse(body)
-  // console.log(body.data[0].abv);
-  console.log(body.data);
+// function apiCallBeer(beer, cb) {
+//   request("https://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=" + mikeKey + "&format=json",
+//     function cb(error, response, body){
+//       body = JSON.parse(body)
+//       // console.log(beerBody.data[0].abv);
+//       // return (beerBody.data[0].abv)
+//   })
+//   return cb()
+// }
 
-})
+//   return requestPromise()
+//     .then(beerData => beerData)
+//     .catch(err => console.log(err));
+// }
+let getBeer = []
+
+function apiCallBeer(beer) {
+  const data = {
+    uri: "https://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=" + mikeKey + "&format=json",
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  rp(data)
+    .then(function (body) {
+      //   console.log(body.data[0].abv);
+      body = body.data[0].abv
+      getBeer.splice(0, getBeer.length);
+        getBeer.push(body)
+    })
+    .catch(function (err) {
+      console.log(err);
+  });
+  return getBeer
 }
-
-
 // request for brewery
 function apiCallBrewery(brewery) {request("https://api.brewerydb.com/v2/search?q=" + brewery + "&type=brewery&key=" + mikeKey + "&format=json",
 function (error, response, body){
--- getInput.js
+++ getInput.js
@@ -14,6 +14,11 @@ $( "#searchBeer" ).unbind().click(function() {
   let value = $("#input").val()
    console.log(value);
    apiCallBeer(value);
+   let beer = apiCallBeer()
+  setTimeout(function () {
+      console.log(beer);
+       $(".output").append(beer)
+  }, 2000);
 });
 $( "#getDetails" ).unbind().click(function() {
    console.log( "worked" );
