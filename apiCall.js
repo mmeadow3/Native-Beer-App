54f972df55196ed3372ac0ef067c77be3fa29654
Mike Meadows <mmeadow33@gmail.com>
2016-09-19 21:25:06 -0500

    fully functional, adding styling

--- apiCall.js
+++ apiCall.js
@@ -21,7 +21,11 @@ const mikeKey = require("./apiKey")
 //     .then(beerData => beerData)
 //     .catch(err => console.log(err));
 // }
-let beerData = []
+let beerData = [];
+let beerName = [];
+let beerDesc = [];
+
+
 
 function apiCallBeer(beer) {
   const data = {
@@ -33,37 +37,73 @@ function apiCallBeer(beer) {
     .then(function (body) {
       //   console.log(body.data[0].abv);
       // body = body.data[0].name body.data[0].name
+      // console.log(body);
       let name = body.data[0].name
       let abv = body.data[0].abv
       let decsription = body.data[0].description
       beerData.splice(0, beerData.length);
-        beerData.push(name, abv, decsription)
+        beerData.push(abv + "%")
+      beerName.splice(0, beerName.length);
+        beerName.push(name)
+      beerDesc.splice(0, beerDesc.length);
+        beerDesc.push(decsription)
     })
     .catch(function (err) {
       console.log(err);
   });
-  return beerData
+  return beerData, beerName, beerDesc
 }
-///////request for brewery///////////////////////
-let getBrewery = []
 
-function apiCallBrewery(brewery) {
+//////////get details////////////
+let beerDetails = [];
+let beerGlassware = [];
+let beerImage = [];
+let beerAvailability = [];
+
+function getDetails(beer) {
   const data = {
-    uri: "https://api.brewerydb.com/v2/search?q=" + brewery + "&type=beer&key=" + mikeKey + "&format=json",
+    uri: "https://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=" + mikeKey + "&format=json",
     json: true // Automatically parses the JSON string in the response
   };
 
   rp(data)
     .then(function (body) {
-      //   console.log(body.data[0].abv);
-      body = body.data[0].abv
-      getBrewery.splice(0, getBrewery.length);
-        getBrewery.push(body)
+      // console.log(body.data[0].labels.medium);
+      let glassware = body.data[0].glass.name
+      let image = body.data[0].labels.medium
+      let availability = body.data[0].available.name
+      beerImage.splice(0, beerImage.length);
+      beerImage.push(image.toString())
+      beerGlassware.splice(0, beerGlassware.length);
+        beerGlassware.push("Recommended Glassware: " + glassware)
+      beerAvailability.splice(0, beerAvailability.length);
+        beerAvailability.push("Availability: " + availability)
     })
     .catch(function (err) {
       console.log(err);
   });
-  return getBrewery
+  return beerDetails, beerGlassware, beerImage, beerAvailability
 }
+///////request for brewery///////////////////////
+// let getBrewery = []
 
-module.exports =  {apiCallBeer, apiCallBrewery}
+// function apiCallBrewery(brewery) {
+//   const data = {
+//     uri: "https://api.brewerydb.com/v2/search?q=" + brewery + "&type=beer&key=" + mikeKey + "&format=json",
+//     json: true // Automatically parses the JSON string in the response
+//   };
+
+//   rp(data)
+//     .then(function (body) {
+//       //   console.log(body.data[0].abv);
+//       body = body.data[0].abv
+//       getBrewery.splice(0, getBrewery.length);
+//         getBrewery.push(body)
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//   });
+//   return getBrewery
+// }
+
+module.exports =  {apiCallBeer, getDetails, beerData, beerName, beerDesc, beerGlassware, beerAvailability, beerImage}
--- getInput.js
+++ getInput.js
@@ -1,32 +1,45 @@
 "use strict";
 
 const $ = require("jquery")
-const {apiCallBeer, apiCallBrewery} = require("./apiCall")
+const {apiCallBeer, getDetails, beerAvailability, beerGlassware, beerData, beerDesc, beerName, beerImage} = require("./apiCall")
 
 ///////click events for buttons/////////////////
 
-$( "#searchBrewery" ).unbind().click(function() {
-  let value = $("#input").val()
-   console.log(value);
-   apiCallBrewery(value);
-   let brewery = apiCallBrewery()
-  setTimeout(function () {
-      console.log(brewery);
-       $(".output").html(brewery)
-  }, 2000);
-});
+
 $( "#searchBeer" ).unbind().click(function() {
   let value = $("#input").val()
-   console.log(value);
-   apiCallBeer(value);
-   let beer = apiCallBeer()
+  apiCallBeer(value);
   setTimeout(function () {
-      console.log(beer);
-       $(".output").html(beer)
-  }, 2000);
+    $(".outputName").html(beerName)
+    $(".outputAbv").html(beerData)
+    $(".outputDesc").html(beerDesc)
+    $("#input").val("");
+  }, 1000);
 });
-$( "#getDetails" ).unbind().click(function() {
-   console.log( "worked" );
+
+// $( "#searchBrewery" ).unbind().click(function() {
+//   let value = $("#input").val()
+//    console.log(value);
+//    apiCallBrewery(value);
+//    let brewery = apiCallBrewery()
+//   setTimeout(function () {
+//       console.log(brewery);
+//        $(".output").html(brewery)
+//   }, 2000);
+// });
+
+
+
+
+$( "#getDetails" ).click(function() {
+  getDetails(beerName)
+  setTimeout(function () {
+  $(".availabilityOutput").html(beerAvailability)
+  $(".glasswareOutput").html(beerGlassware)
+  $(".imageOutput").html('<img src="'+beerImage+'"/>')
+  }, 1000);
 });
+
+
 
 ///////////////////////////////////////////////////
